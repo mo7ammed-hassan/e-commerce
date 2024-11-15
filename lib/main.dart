ce98bf7c6d74c8d0cbb751a1fcf5f3e4ee34a7cd
mohamedhassan <mohamed7github@gmail.com>
2024-11-15 19:47:11 +0200

    create app theme for text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/utils/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +14,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'T-Store',
       themeMode: ThemeMode.system,
-      theme: ThemeData(),
-      darkTheme: ThemeData(),
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
     );
   }
 }
