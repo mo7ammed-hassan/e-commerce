3ca4daf3e827b5df86113450fd07b04e381dc0b5
mohamedhassan <mohamed7github@gmail.com>
2024-11-17 00:41:53 +0200

    refactor code and create home page

--- lib/common/manager/cubits/navigation_menu_cubit.dart
+++ lib/common/manager/cubits/navigation_menu_cubit.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:t_store/features/shop/features/home/presentation/pages/home_page.dart';
 
 class NavigationMenuCubit extends Cubit<int> {
   NavigationMenuCubit() : super(0);
 
   final List<Widget> screens = [
-    Container(color: Colors.red),
+    const HomePage(),
     Container(color: Colors.green),
     Container(color: Colors.yellow),
     Container(color: Colors.brown),
--- lib/common/widgets/navigation_menu/bottom_navigation_bar.dart
+++ lib/common/widgets/navigation_menu/bottom_navigation_bar.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/manager/cubits/navigation_menu_cubit.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class TBottomNavigationBar extends StatelessWidget {
   const TBottomNavigationBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isDark = THelperFunctions.isDarkMode(context);
     return BlocBuilder<NavigationMenuCubit, int>(
       builder: (context, state) {
         return NavigationBar(
@@ -15,6 +18,10 @@ class TBottomNavigationBar extends StatelessWidget {
           elevation: 0,
           selectedIndex: state,
           onDestinationSelected: context.read<NavigationMenuCubit>().setScreen,
+          backgroundColor: isDark ? TColors.black : TColors.white,
+          indicatorColor: isDark
+              ? TColors.white.withOpacity(0.1)
+              : TColors.black.withOpacity(0.1),
           destinations: const [
             NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
             NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
