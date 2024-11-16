ee6b31a6f6dbb4d72891d06a99d21306a8dac56e
mohamedhassan <mohamed7github@gmail.com>
2024-11-16 13:55:42 +0200

    git add finish setup utils folder

---
+++ lib/utils/loaders/circular_loader.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+
+/// A circular loader widget with customizable foreground and background colors.
+class TCircularLoader extends StatelessWidget {
+  const TCircularLoader({
+    super.key,
+    this.foregroundColor = TColors.white,
+    this.backgroundColor = TColors.primaryColor,
+  });
+
+  final Color? foregroundColor;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(TSizes.lg),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        shape: BoxShape.circle,
+      ), // Circular background
+      child: Center(
+        child: CircularProgressIndicator(
+          color: foregroundColor,
+          backgroundColor: Colors.transparent,
+        ), // Circular loader
+      ),
+    );
+  }
+}
