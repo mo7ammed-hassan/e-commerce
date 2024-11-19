68a601998a963f74deae77b8b27d0f05faad2dc1
mohamedhassan <mohamed7github@gmail.com>
2024-11-19 13:06:30 +0200

    create wishlist page

--- lib/common/manager/cubits/navigation_menu_cubit.dart
+++ lib/common/manager/cubits/navigation_menu_cubit.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:t_store/features/shop/features/home/presentation/pages/home_page.dart';
 import 'package:t_store/features/shop/features/store/presentation/pages/store_page.dart';
+import 'package:t_store/features/shop/features/wishlist/presentation/pages/wishlist_page.dart';
 
 class NavigationMenuCubit extends Cubit<int> {
   NavigationMenuCubit() : super(0);
@@ -9,7 +10,7 @@ class NavigationMenuCubit extends Cubit<int> {
   final List<Widget> screens = [
     const HomePage(),
     const StorePage(),
-    Container(color: Colors.yellow),
+    const WishlistPage(),
     Container(color: Colors.brown),
   ];
 
