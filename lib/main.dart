86de21bf695d9947726217e1065aef0118db91d5
mohamedhassan <mohamed7github@gmail.com>
2024-11-23 15:14:21 +0200

    update applicationid in build.gradle

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:t_store/firebase_options.dart';
 import 'package:t_store/utils/theme/theme.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
