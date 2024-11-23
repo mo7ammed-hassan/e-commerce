8311e778f5075232321a379782ebcf7374ffb99b
mohamedhassan <mohamed7github@gmail.com>
2024-11-23 14:42:35 +0200

    setup firebbase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:t_store/features/authentication/presentation/pages/onboarding_page.dart';
+import 'package:t_store/firebase_options.dart';
 import 'package:t_store/utils/theme/theme.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
