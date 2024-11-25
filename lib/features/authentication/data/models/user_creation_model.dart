e5b8040179071f5f9cbf4f221257c564ead1b9c3
mohamedhassan <mohamed7github@gmail.com>
2024-11-25 20:34:48 +0200

    finish impl signup method in auth services

--- lib/features/authentication/data/models/user_creation_model.dart
+++ lib/features/authentication/data/models/user_creation_model.dart
@@ -1,3 +1,4 @@
+
 class UserCreationModel {
   final String firstName;
   final String lastName;
@@ -14,4 +15,26 @@ class UserCreationModel {
     this.password,
     this.userPhone,
   );
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'firstName': firstName,
+      'lastName': lastName,
+      'userEmail': userEmail,
+      'userName': userName,
+      'password': password,
+      'userPhone': userPhone,
+    };
+  }
+
+  factory UserCreationModel.fromMap(Map<String, dynamic> map) {
+    return UserCreationModel(
+      map['firstName'] as String,
+      map['lastName'] as String,
+      map['userEmail'] as String,
+      map['userName'] as String,
+      map['password'] as String,
+      map['userPhone'] as String,
+    );
+  }
 }
--- lib/features/authentication/data/source/authentication_source/authentication_firebase_services.dart
+++ lib/features/authentication/data/source/authentication_source/authentication_firebase_services.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:t_store/features/authentication/data/models/user_creation_model.dart';
 import 'package:t_store/features/authentication/data/models/user_signin_model.dart';
 
@@ -32,11 +34,36 @@ class AuthenticationFirebaseServicesImpl
   }
 
   @override
-  Future<Either> signup(UserCreationModel usetCreationModel) {
-    // TODO: implement signup
+  Future<Either> signup(UserCreationModel usetCreationModel) async {
+    try {
+      // create user
+      final UserCredential credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: usetCreationModel.userEmail,
+        password: usetCreationModel.password,
+      );
 
-    //TODO: after signup we should Storage user in firebase
-    throw UnimplementedError();
+      // stor user
+      await FirebaseFirestore.instance
+          .collection('Users')
+          .doc(credential.user!.uid)
+          .set(
+            usetCreationModel.toMap(),
+          );
+      return const Right(
+        'Your Account has been created! Verify email to continue',
+      );
+    } on FirebaseAuthException catch (e) {
+      String message = '';
+      if (e.code == 'weak-password') {
+        message = 'The password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        message = 'The account already exists for that email.';
+      }
+      return Left(message);
+    } catch (e) {
+      return const Left('There was an error, please try again');
+    }
   }
 
   @override
