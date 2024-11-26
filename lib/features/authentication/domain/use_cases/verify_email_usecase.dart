d541b3e91a442e67813775b81bfd62f94fce2277
mohamedhassan <mohamed7github@gmail.com>
2024-11-26 15:15:08 +0200

    create VerifyEmailUsecase

--- lib/features/authentication/domain/use_cases/verify_email_usecase.dart
+++ lib/features/authentication/domain/use_cases/verify_email_usecase.dart
@@ -1,10 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:t_store/common/core/usecases/use_cases.dart';
+import 'package:t_store/features/authentication/domain/repository/authentication_repository.dart';
+import 'package:t_store/service_locator.dart';
 
 class VerifyEmailUsecase extends UseCases<Either, String> {
   @override
-  Future<Either> call({String? params}) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either> call({String? params}) async {
+    return await getIt<AuthenticationRepository>().verifyEmail(email: params!);
   }
 }
