9da664e682872ed52fa9034cc2f459a83a950d89
mohamedhassan <mohamed7github@gmail.com>
2024-12-08 14:27:09 +0200

    finish  BannerCubit

--- lib/features/shop/features/home/presentation/cubits/banner/banner_cubit.dart
+++ lib/features/shop/features/home/presentation/cubits/banner/banner_cubit.dart
@@ -1,8 +1,31 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:t_store/features/shop/features/home/domain/entites/banner_entity.dart';
+import 'package:t_store/features/shop/features/home/domain/use_cases/banner_use_case.dart';
+import 'package:t_store/service_locator.dart';
 
 part 'banner_state.dart';
 
 class BannerCubit extends Cubit<BannerState> {
-  BannerCubit() : super(BannerInitial());
+  BannerCubit() : super(const BannerInitial());
+
+  final List<BannerEntity> allBanners = [];
+
+  void getAllCategories() async {
+    emit(const BannerLoadingState());
+
+    // fetch banners
+    var result = await getIt<BannerUseCase>().call();
+
+    result.fold(
+      (error) {
+        emit(BannerFailureState(error));
+      },
+      (categories) {
+        allBanners.clear();
+        allBanners.addAll(categories);
+
+        emit(BannerLoadedState(allBanners));
+      },
+    );
+  }
 }
--- lib/features/shop/features/home/presentation/cubits/banner/banner_state.dart
+++ lib/features/shop/features/home/presentation/cubits/banner/banner_state.dart
@@ -1,6 +1,25 @@
 part of 'banner_cubit.dart';
 
-@immutable
-sealed class BannerState {}
+sealed class BannerState {
+  const BannerState();
+}
 
-final class BannerInitial extends BannerState {}
+final class BannerInitial extends BannerState {
+  const BannerInitial();
+}
+
+final class BannerLoadingState extends BannerState {
+  const BannerLoadingState();
+}
+
+final class BannerLoadedState extends BannerState {
+  final List<BannerEntity> allBanners;
+
+  const BannerLoadedState(this.allBanners);
+}
+
+final class BannerFailureState extends BannerState {
+  final String errorMessage;
+
+  const BannerFailureState(this.errorMessage);
+}
